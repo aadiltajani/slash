ff76ceeb6b5739e7f53cb09cb50d265d8be264ff
Aadil Tajani <aadil.tajani@agami-tech.com>
2022-12-03 20:18:40 -0500

    changed parser and shopping list

--- src/google_scrapper.py
+++ src/google_scrapper.py
@@ -9,19 +9,18 @@ def searchGoogle(query):
     # CONFIGURATION
     headers = {
         "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
     }
     # query = "lenovo thinkpad"
-    params = {"q": query, "hl": "en", 'gl': 'us', 'tbm': 'shop'}
+    # params = {"q": query, "hl": "en", 'gl': 'us', 'tbm': 'shop'}
     freeDelivery = False
-    url = 'https://www.google.com/search'
-
+    url = r'https://www.google.com/search?hl=en&tbm=shop&q={}&&sclient=products-cc'.format('+'.join(query.split(' ')))
     # REQUEST
     response = requests.get(url,
-                            params=params,
+                            # params=params,
                             headers=headers)
 
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     # PARSING RESPONSE
     shopping_data_dict = {}
@@ -47,25 +46,47 @@ def searchGoogle(query):
 
     shopping_data_dict.update({"inline_shopping_results": inline_results})
 
-    for shopping_result in soup.select('.sh-dgr__content'):
-        title = shopping_result.select_one('.Lq5OHe.eaGTj h4').text
-        product_link = f"https://www.google.com{shopping_result.select_one('.Lq5OHe.eaGTj')['href']}"
-        source = shopping_result.select_one('.IuHnof').text
-        price = shopping_result.select_one('span.kHxwFf span').text
-        image = shopping_result.findAll('img')[0].attrs['src']
+    # for shopping_result in soup.select('.sh-dgr__content'):
+    #     title = shopping_result.select_one('.Lq5OHe.eaGTj h4').text
+    #     product_link = f"https://www.google.com{shopping_result.select_one('.Lq5OHe.eaGTj')['href']}"
+    #     source = shopping_result.select_one('.IuHnof').text
+    #     price = shopping_result.select_one('span.kHxwFf span').text
+    #     image = shopping_result.findAll('img')[0].attrs['src']
+    #
+    #     try:
+    #         rating = shopping_result.select_one('.Rsc7Yb').text
+    #     except:
+    #         rating = None
+    #
+    #     try:
+    #         reviews = shopping_result.select_one('.Rsc7Yb').next_sibling.next_sibling
+    #     except:
+    #         reviews = None
+    #
+    #     try:
+    #         delivery = shopping_result.select_one('.vEjMR').text
+    #     except:
+    #         delivery = None
+    res = soup.find_all('div', class_='sh-dgr__content')
+    for r in res:
+        title = r.select_one('.tAxDx').text
+        product_link = f"https://www.google.com{r.select_one('.Lq5OHe.eaGTj')['href']}"
+        source = r.select_one('.IuHnof').text
+        price = r.select_one('.OFFNJ').text
+        image = r.find('img')
 
         try:
-            rating = shopping_result.select_one('.Rsc7Yb').text
+            rating = r.select_one('.QIrs8').text
         except:
             rating = None
 
         try:
-            reviews = shopping_result.select_one('.Rsc7Yb').next_sibling.next_sibling
+            reviews = r.select_one('.Rsc7Yb').next_sibling.next_sibling
         except:
             reviews = None
 
         try:
-            delivery = shopping_result.select_one('.vEjMR').text
+            delivery = r.select_one('.vEjMR').text
         except:
             delivery = None
 
--- src/web/app.py
+++ src/web/app.py
@@ -1,9 +1,6 @@
 import sys
 sys.path.append('../')
 from flask import Flask, render_template, request
-from scraper import searchAmazon
-from scraper import searchTarget
-from scraper import searchWalmart
 from google_scrapper import searchGoogle
 
 app = Flask(__name__)
@@ -17,32 +14,11 @@ def home():
 @app.route('/get_products', methods=['POST'])
 def get_products():
     input = request.form.values()
-    print(input)
-    ans = str(input)
+    val = list(input)[0]
+    print('Searching for', val)
 
-    # products1 = searchAmazon(input,False)
-    products1 = {}
-    products1 = searchGoogle(input)
+    products1 = searchGoogle(val)
 
-    # output = model.predict(input)
-    # output = round(output[0],3)
-    # ans = audioinput(r'../data/audiofiles/' + input.filename)
-    # # ans = audioinput(input)
-    # text_dic = speech_to_text(r'../data/chunks')
-
-    # text = ''
-    # for i in text_dic.values():
-    #     text += ' ' + i
-    # ans = ans + "<br><br>"
-    # ans = ans + "<br>Speech to Text:" + str(text)
-    # ans = ans + "<br><br>Sentiment Analysis:" + str(sentiment_scores(text))
-    # ans = ans + "<br><br>Emotion Analysis:" + str(emotion_detection(text))
-    # ans = ans + "<br><br>Keywords:" + str(getkewords(text))
-    # ans = ans + "<br><br>Word Count:" + str(wordcount(text_dic))
-    # # return render_template('index.html')
-
-    # # return render_template('index.html', analysis_text = 'Weight should be {}lbs.'.format(output))
-    # print(ans)
     return render_template('index.html', res1=products1['inline_shopping_results'], res2 = products1['shopping_results'])
 
 
