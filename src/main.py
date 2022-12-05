f72f106c71e622804bb2076a3cab61fe37d6f769
Dhruvish-Patel <dhruvishpatel02@gmail.com>
2022-12-05 02:30:59 -0500

    updated main.py and test.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from google_scrapper import searchGoogle
 
 # searching without sorting, retrieving 10 items
-googleSearch('iphone 14', False, 10)
+searchGoogle('iphone 14', False, 10)
 
 # searching with sorting, retrieving 10 items
-googleSearch('cake', True, 20)
+searchGoogle('cake', True, 20)
 
 # searching without sorting, retrieving all items
-googleSearch('charger', False)
+searchGoogle('charger', False)
 
 # searching with sorting, retrieving all items
-googleSearch('milk', True)
+searchGoogle('milk', True)
