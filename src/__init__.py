1cb4f7e7c538af0f2e09e437b45bf19bc5479bb0
asingh0404 <112002096+asingh0404@users.noreply.github.com>
2022-12-04 23:54:14 -0500

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+# This is the Auto Generated Document for our project 2 Slash
+([GitHub Repository](https://github.com/aadiltajani/slash))
+You can go through the following modules:
+1) google_scrapper:
+https://aadiltajani.github.io/slash/google_scrapper.html
+
+Working example:
+[link](https://github.com/aadiltajani/slash/actions/runs/3216582722)
+.. include:: ../README.md
+"""
