824ee7bf473487357d5b147c757952757ade58a6
Dhruvish-Patel <dhruvishpatel02@gmail.com>
2022-12-05 02:51:31 -0500

    updated test/test_cases.py

--- test/test_cases.py
+++ test/test_cases.py
@@ -3,14 +3,18 @@ import sys
 sys.path.append('./src')
 from google_scrapper import searchGoogle
 
-# searching without sorting, retrieving 10 items
-searchGoogle('iphone 14', False, 10)
+def test_cases():
 
-# searching with sorting, retrieving 10 items
-searchGoogle('cake', True, 20)
+    # searching without sorting, retrieving 10 items
+    searchGoogle('iphone 14', False, 10)
 
-# searching without sorting, retrieving all items
-searchGoogle('charger', False)
+    # searching with sorting, retrieving 10 items
+    searchGoogle('cake', True, 20)
 
-# searching with sorting, retrieving all items
-searchGoogle('milk', True)
+    # searching without sorting, retrieving all items
+    searchGoogle('charger', False)
+
+    # searching with sorting, retrieving all items
+    searchGoogle('milk', True)
+
+test_cases()
